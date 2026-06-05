928fa7794ef916a89614f7a2365f87f312490a95
Jesus Rodriguez Marin <jrodriguezmarin21@gmail.com>
2026-06-05 11:43:37 +0200

    feat(navigation): close mobile menu when navigation link is selected

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 const menuToggle = document.querySelector(".menu-toggle");
 const nav = document.querySelector(".nav");
+const navLinks = document.querySelectorAll(".nav a");
 
 menuToggle.addEventListener("click", () => {
   nav.classList.toggle("active");
+});
+
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
 });
